a2abe4aa483c696a2889ab6a6a3eec1e86b924fb
Ahmad Riad Al-adaileh <128195215+ahmadriad@users.noreply.github.com>
2023-05-16 00:22:21 +0400

    Add files via upload

---
+++ Exploratory-Data-Analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' Predicting the quality of the wine',
+    author='Ahmad',
+    license='',
+)
